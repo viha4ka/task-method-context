d18da4f638da7473bd55cf5332f4bac66160a7d5
viha4ka <vikusha2012@mail.ru>
2022-06-03 14:37:08 +0500

    Готово

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -1,5 +1,4 @@
 import { EventEmitter } from './EventEmitter';
-
 /*
 1) В методе subscribe подпишитесь на событие click с помощью EventEmitter.on(eventName, callback).
 В обработчике нужно увеличивать значение поля count на 1
@@ -8,18 +7,26 @@ import { EventEmitter } from './EventEmitter';
  */
 export const obj = {
     count: 0,
-    subscribe() {},
-    unsubscribe() {},
+    addCount() {
+        obj.count++;
+    },
+
+    subscribe() {
+        EventEmitter.on('click', obj.addCount);
+    },
+    unsubscribe() {
+        EventEmitter.off('click', obj.addCount);
+    },
 };
 
 /*
-Сделайте так, чтобы метод first вызывал метод second со своими аргументами, но в обратном порядке:
-
-obj1.first(1, 2, 3);
+@@ -19,7 +27,9 @@ obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
 export const obj1 = {
-    first(...args) {},
+    first(...args) {
+        obj1.second(...args.reverse());
+    },
     second() {
         // здесь ничего писать не нужно
     },
